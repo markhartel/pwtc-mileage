daa52ec1f464a5b7e983efabec9e34a1ce637c38
Mark Hartel <mark_hartel@hotmail.com>
2016-10-25 17:54:16 -0700

    Create tables and views

--- class.pwtcmileage.php
+++ class.pwtcmileage.php
@@ -6,6 +6,19 @@ class PwtcMileage {
 	const MILEAGE_TABLE = 'pwtc_ride_mileage';
 	const LEADER_TABLE = 'pwtc_ride_leaders';
 
+	const LT_MILES_VIEW = 'pwtc_lt_miles_vw';
+	const YTD_MILES_VIEW = 'pwtc_ytd_miles_vw';
+	const LY_MILES_VIEW = 'pwtc_ly_miles_vw';
+	const LY_LT_MILES_VIEW = 'pwtc_ly_lt_miles_vw';
+	const YBL_LT_MILES_VIEW = 'pwtc_ybl_lt_miles_vw';
+	const LY_LT_ACHVMNT_VIEW = 'pwtc_ly_lt_achvmnt_vw';
+	const YTD_RIDES_LED_VIEW = 'pwtc_ytd_rides_led_vw';
+	const LY_RIDES_LED_VIEW = 'pwtc_ly_rides_led_vw';
+	const YTD_LED_VIEW = 'pwtc_ytd_led_vw';
+	const LY_LED_VIEW = 'pwtc_ly_led_vw';
+	const YTD_RIDES_VIEW = 'pwtc_ytd_rides_vw';
+	const LY_RIDES_VIEW = 'pwtc_ly_rides_vw';
+
     private static $initiated = false;
 
 	public static function init() {
@@ -53,15 +66,29 @@ class PwtcMileage {
 	public static function plugin_activation() {
 		error_log( 'PWTC Mileage plugin activated' );
 		if ( version_compare( $GLOBALS['wp_version'], PWTC_MILEAGE__MINIMUM_WP_VERSION, '<' ) ) {
-			$message = '<strong>'.sprintf('PWTC Mileage %s requires WordPress %s or higher.', PWTC_MILEAGE__VERSION, PWTC_MILEAGE__MINIMUM_WP_VERSION ).'</strong> '.sprintf('Please <a href="%1$s">upgrade WordPress</a> to a current version.', 'https://codex.wordpress.org/Upgrading_WordPress');
-			PwtcMileage::bail_on_activation( $message );
+			//TODO: Implement version check fail abort
 		}
 		self::create_db_tables();
+		self::create_db_views();
+	}
+
+	public static function plugin_deactivation( ) {
+		error_log( 'PWTC Mileage plugin deactivated' );
+	}
+
+	public static function plugin_uninstall() {
+		error_log( 'PWTC Mileage plugin uninstall' );	
+		self::drop_db_views();	
+		self::drop_db_tables();	
 	}
 
 	public static function create_db_tables( ) {
 		global $wpdb;
 		$member_table = $wpdb->prefix . self::MEMBER_TABLE;
+		$ride_table = $wpdb->prefix . self::RIDE_TABLE;
+		$mileage_table = $wpdb->prefix . self::MILEAGE_TABLE;
+		$leader_table = $wpdb->prefix . self::LEADER_TABLE;
+		
 		$result = $wpdb->query('create table if not exists ' . $member_table . 
 			' (member_id VARCHAR(5) NOT NULL,' .
 			' last_name TEXT NOT NULL,' . 
@@ -71,63 +98,198 @@ class PwtcMileage {
 		if (false === $result) {
 			error_log( 'Could not create table ' . $member_table . ': ' . $wpdb->last_error);
 		}
+
+		$result = $wpdb->query('create table if not exists ' . $ride_table .
+			' (ID BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,' .
+			' title TEXT NOT NULL,' .
+			' date DATE NOT NULL,' . 
+			' post_id BIGINT UNSIGNED,' . 
+			' constraint pk_' . $ride_table . ' PRIMARY KEY (ID))');
+		if (false === $result) {
+			error_log( 'Could not create table ' . $ride_table . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create table if not exists ' . $mileage_table . 
+			' (member_id VARCHAR(5) NOT NULL,' . 
+			' ride_id BIGINT UNSIGNED NOT NULL,' . 
+			' mileage INT UNSIGNED NOT NULL,' . 
+			' constraint pk_' . $mileage_table . ' PRIMARY KEY (member_id, ride_id),' . 
+			' constraint fk_' . $mileage_table . '_member_id FOREIGN KEY (member_id) REFERENCES ' . $member_table . ' (member_id),' . 
+			' constraint fk_' . $mileage_table . '_ride_id FOREIGN KEY (ride_id) REFERENCES ' . $ride_table . ' (ID))');
+		if (false === $result) {
+			error_log( 'Could not create table ' . $mileage_table . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create table if not exists ' . $leader_table . 
+			' (member_id VARCHAR(5) NOT NULL,' . 
+			' ride_id BIGINT UNSIGNED NOT NULL,' . 
+			' rides_led INT UNSIGNED NOT NULL,' . 
+			' constraint pk_' . $leader_table . ' PRIMARY KEY (member_id, ride_id),' . 
+			' constraint fk_' . $leader_table . '_member_id FOREIGN KEY (member_id) REFERENCES ' . $member_table . ' (member_id),' . 
+			' constraint fk_' . $leader_table . '_ride_id FOREIGN KEY (ride_id) REFERENCES ' . 
+			$ride_table . ' (ID))');
+		if (false === $result) {
+			error_log( 'Could not create table ' . $leader_table . ': ' . $wpdb->last_error);
+		}
 	}
 
 	public static function create_db_views( ) {
+		global $wpdb;
+		$member_table = $wpdb->prefix . self::MEMBER_TABLE;
+		$ride_table = $wpdb->prefix . self::RIDE_TABLE;
+		$mileage_table = $wpdb->prefix . self::MILEAGE_TABLE;
+		$leader_table = $wpdb->prefix . self::LEADER_TABLE;
+
+		$result = $wpdb->query('create or replace view ' . self::LT_MILES_VIEW . 
+			' (member_id, first_name, last_name, mileage)' . 
+			' as select c.member_id, c.first_name, c.last_name, SUM(m.mileage)' . 
+			' from ' . $member_table . ' as c inner join ' . $mileage_table . ' as m on c.member_id = m.member_id' . 
+			' group by m.member_id');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::LT_MILES_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::YTD_MILES_VIEW . 
+			' (member_id, first_name, last_name, mileage)' . 
+			' as select c.member_id, c.first_name, c.last_name, SUM(m.mileage)' . 
+			' from ((' . $mileage_table . ' as m inner join ' . $member_table . ' as c on c.member_id = m.member_id)' . 
+			' inner join ' . $ride_table . ' as r on m.ride_id = r.ID)' . 
+			' where r.date >= DATE_FORMAT(CURDATE(), \'%Y-01-01\')' . 
+			' group by m.member_id');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::YTD_MILES_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::LY_MILES_VIEW . 
+			' (member_id, first_name, last_name, mileage)' . 
+			' as select c.member_id, c.first_name, c.last_name, SUM(m.mileage)' . 
+			' from ((' . $mileage_table . ' as m inner join ' . $member_table . ' as c on c.member_id = m.member_id)' . 
+			' inner join ' . $ride_table . ' as r on m.ride_id = r.ID)' . 
+			' where r.date between DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), \'%Y-01-01\')' . 
+			' and DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), \'%Y-12-31\')' . 
+			' group by m.member_id');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::LY_MILES_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::LY_LT_MILES_VIEW . 
+			' (member_id, first_name, last_name, mileage)' . 
+			' as select c.member_id, c.first_name, c.last_name, SUM(m.mileage)' . 
+			' from ((' . $mileage_table . ' as m inner join ' . $member_table . ' as c on c.member_id = m.member_id)' . 
+			' inner join ' . $ride_table . ' as r on m.ride_id = r.ID)' . 
+			' where r.date < DATE_FORMAT(CURDATE(), \'%Y-01-01\')' . 
+			' group by m.member_id');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::LY_LT_MILES_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::YBL_LT_MILES_VIEW . 
+			' (member_id, first_name, last_name, mileage)' . 
+			' as select c.member_id, c.first_name, c.last_name, SUM(m.mileage)' . 
+			' from ((' . $mileage_table . ' as m inner join ' . $member_table . ' as c on c.member_id = m.member_id)' . 
+			' inner join ' . $ride_table . ' as r on m.ride_id = r.ID)' . 
+			' where r.date < DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), \'%Y-01-01\')' . 
+			' group by m.member_id');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::YBL_LT_MILES_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::LY_LT_ACHVMNT_VIEW . 
+			' (member_id, first_name, last_name, mileage, achievement)' . 
+			' as select a.member_id, a.first_name, a.last_name, a.mileage, concat(floor(a.mileage/10000),\'0K\')' . 
+			' from ' . self::LY_LT_MILES_VIEW . ' as a inner join ' . self::YBL_LT_MILES_VIEW . ' as b on a.member_id = b.member_id' . 
+			' where floor(a.mileage/10000) > floor(b.mileage/10000)');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::LY_LT_ACHVMNT_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::YTD_RIDES_LED_VIEW . 
+			' (title, date, member_id)' . 
+			' as select r.title, r.date, l.member_id' . 
+			' from ' . $ride_table . ' as r inner join ' . $leader_table . ' as l on r.ID = l.ride_id' . 
+			' where r.date >= DATE_FORMAT(CURDATE(), \'%Y-01-01\')' . 
+			' order by r.date');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::YTD_RIDES_LED_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::LY_RIDES_LED_VIEW . 
+			' (title, date, member_id)' . 
+			' as select r.title, r.date, l.member_id' .
+			' from ' . $ride_table . ' as r inner join ' . $leader_table . ' as l on r.ID = l.ride_id' . 
+			' where r.date between DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), \'%Y-01-01\')' . 
+			' and DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), \'%Y-12-31\')' . 
+			' order by r.date');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::LY_RIDES_LED_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::YTD_LED_VIEW . 
+			' (member_id, first_name, last_name, rides_led)' . 
+			' as select c.member_id, c.first_name, c.last_name, SUM(l.rides_led)' . 
+			' from ((' . $leader_table . ' as l inner join ' . $member_table . ' as c on c.member_id = l.member_id)' . 
+			' inner join ' . $ride_table . ' as r on l.ride_id = r.ID)' . 
+			' where r.date >= DATE_FORMAT(CURDATE(), \'%Y-01-01\')' . 
+			' group by l.member_id');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::YTD_LED_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::LY_LED_VIEW . 
+			'(member_id, first_name, last_name, rides_led)' . 
+			' as select c.member_id, c.first_name, c.last_name, SUM(l.rides_led)' . 
+			' from ((' . $leader_table . ' as l inner join ' . $member_table . ' as c on c.member_id = l.member_id)' . 
+			' inner join ' . $ride_table . ' as r on l.ride_id = r.ID)' . 
+			' where r.date between DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), \'%Y-01-01\')' . 
+			' and DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), \'%Y-12-31\')' . 
+			' group by l.member_id');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::LY_LED_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::YTD_RIDES_VIEW . 
+			' (title, date, mileage, member_id)' . 
+			' as select r.title, r.date, m.mileage, m.member_id' . 
+			' from ' . $ride_table . ' as r inner join ' . $mileage_table . ' as m on r.ID = m.ride_id' . 
+			' where r.date >= DATE_FORMAT(CURDATE(), \'%Y-01-01\')' . 
+			' order by r.date'); 
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::YTD_RIDES_VIEW . ': ' . $wpdb->last_error);
+		}
+
+		$result = $wpdb->query('create or replace view ' . self::LY_RIDES_VIEW . 
+			' (title, date, mileage, member_id)' . 
+			' as select r.title, r.date, m.mileage, m.member_id' . 
+			' from ' . $ride_table . ' as r inner join ' . $mileage_table . ' as m on r.ID = m.ride_id' . 
+			' where r.date between DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), \'%Y-01-01\')' . 
+			' and DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), \'%Y-12-31\')' . 
+			' order by r.date');
+		if (false === $result) {
+			error_log( 'Could not create view ' . self::LY_RIDES_VIEW . ': ' . $wpdb->last_error);
+		}
 	}
 
 	public static function drop_db_tables( ) {
+		global $wpdb;
+		$member_table = $wpdb->prefix . self::MEMBER_TABLE;
+		$ride_table = $wpdb->prefix . self::RIDE_TABLE;
+		$mileage_table = $wpdb->prefix . self::MILEAGE_TABLE;
+		$leader_table = $wpdb->prefix . self::LEADER_TABLE;
+
+		$result = $wpdb->query('drop table if exists ' . $leader_table . ', ' . $mileage_table . ', ' . $ride_table . ', ' . $member_table);
+		if (false === $result) {
+			error_log( 'Could not drop tables: ' . $wpdb->last_error);
+		}
 	}
 
 	public static function drop_db_views( ) {
-	}
-
-	public static function plugin_deactivation( ) {
-		error_log( 'PWTC Mileage plugin deactivated' );
-	}
-
-	public static function plugin_uninstall() {
-		error_log( 'PWTC Mileage plugin uninstall' );		
-	}
-
-	private static function bail_on_activation( $message, $deactivate = true ) {
-?>
-<!doctype html>
-<html>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<style>
-* {
-	text-align: center;
-	margin: 0;
-	padding: 0;
-	font-family: "Lucida Grande",Verdana,Arial,"Bitstream Vera Sans",sans-serif;
-}
-p {
-	margin-top: 1em;
-	font-size: 18px;
-}
-</style>
-<body>
-<p><?php echo $message; ?></p>
-</body>
-</html>
-<?php
-		if ( $deactivate ) {
-			$plugins = get_option( 'active_plugins' );
-			$pwtcmileage = plugin_basename( PWTC_MILEAGE__PLUGIN_DIR . 'pwtc-mileage.php' );
-			$update  = false;
-			foreach ( $plugins as $i => $plugin ) {
-				if ( $plugin === $pwtcmileage ) {
-					$plugins[$i] = false;
-					$update = true;
-				}
-			}
-
-			if ( $update ) {
-				update_option( 'active_plugins', array_filter( $plugins ) );
-			}
+		$result = $wpdb->query('drop view if exists ' . self::LY_RIDES_VIEW . ', ' . self::YTD_RIDES_VIEW . ', ' . self::LY_LED_VIEW . 
+			', ' . self::YTD_LED_VIEW . ', ' . self::LY_RIDES_LED_VIEW . ', ' . self::YTD_RIDES_LED_VIEW . ', ' . self::LY_LT_ACHVMNT_VIEW . 
+			', ' . self::YBL_LT_MILES_VIEW . ', ' . self::LY_LT_MILES_VIEW . ', ' . self::LY_MILES_VIEW . ', ' . self::YTD_MILES_VIEW . 
+			', ' . self::LT_MILES_VIEW);
+		if (false === $result) {
+			error_log( 'Could not drop views: ' . $wpdb->last_error);
 		}
-		exit;
 	}
+
 }
